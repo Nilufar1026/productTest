a6ff342da8ab89eaa95349a90cc6cbbfb2066e5a
Nilufaer Abulimiti <nilux1026@gmail.com>
2024-10-28 11:53:35 +0100

    fix header

---
+++ client/nextapp/src/app/components/header/Header.tsx
@@ -0,0 +1,41 @@
+// import Link from 'next/link';
+import React from "react";
+import styled from "styled-components";
+import Image from "next/image";
+import logo from "../../../../public/assets/logo.png";
+import user from "../../../../public/assets/user.svg";
+import cart from "../../../../public/assets/cart.svg";
+
+type HeaderProps = {};
+
+const HeaderSection = styled.div<HeaderProps>`
+  width: 100%;
+  background-color: "1e1e1e";
+  display: flex;
+  /* justify-content: center; */
+  align-items: center;
+  justify-content: space-between;
+
+  .userSection{
+    .user{
+        margin-right: 0.5rem;
+    }
+  }
+`;
+
+const Header: React.FC<HeaderProps> = (props) => {
+  return (
+    <HeaderSection>
+      <div className="logo">
+        <Image src={logo} alt="logo" height={30}/>
+      </div>
+      <div className="userSection">
+        <Image src={user} alt="user" className="user"/>
+        <Image src={cart} alt="cart"/>
+      </div>
+    
+    </HeaderSection>
+  );
+};
+
+export default Header;
---
+++ client/nextapp/src/app/components/product/Product.tsx
@@ -0,0 +1,14 @@
+// import Link from 'next/link';
+import React from "react";
+import styled from "styled-components";
+import Image from "next/image";
+
+type ProductProps = {};
+
+const ProductSection = styled.div<ProductProps>``;
+
+const Product: React.FC<ProductProps> = (props) => {
+  return <ProductSection></ProductSection>;
+};
+
+export default Product;
